5d2da1fb0c2fc24639dce35593c005ae73c630de
Karim Bourahla <k.bourahla0x@gmail.com>
2023-12-11 09:36:54 +0100

    create models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from app.models import Receipt
 
 # Register your models here.
+
+
+class ReceiptAdmin(admin.ModelAdmin):
+    readonly_fields = ("date_of_purchase",)
+    list_display = ("id", "date_of_purchase", "total_amount", "user")
+
+
+admin.site.register(Receipt, ReceiptAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Receipt(models.Model):
+    store_name = models.CharField(max_length=255)
+    date_of_purchase = models.DateTimeField(auto_now_add=True)
+    item_list = models.TextField()
+    total_amount = models.FloatField()
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
